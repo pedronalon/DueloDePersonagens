373dfe62e3f14db89f761a20e17b081444016572
pedronalon <pedronalon95@gmail.com>
2025-05-29 22:50:20 -0300

    acabouporhj

--- DueloDePersonagens/src/main/java/trabalho/duelodepersonagens/Jogo.java
+++ DueloDePersonagens/src/main/java/trabalho/duelodepersonagens/Jogo.java
@@ -1,4 +1,36 @@
 package trabalho.duelodepersonagens;
 
+import java.util.Scanner;
+
 public class Jogo {
+    public Jogo() {
+        MenuIniciar();
+        MenuSelecionar();
+    }
+
+    private static void MenuSelecionar() {
+        Scanner teclado = new Scanner(System.in);
+        int  ModoDeJogo ;
+        boolean a  = true;
+        System.out.println("Selecione o modo de jogo!\n 1: PvP \n 2:PvE");
+        ModoDeJogo = teclado.nextInt();
+        while (a) {
+            switch (ModoDeJogo) {
+                case 1: //seleciona o pvp
+                    a = false;
+                    break;
+                case 2:   // seleciona o pve
+                    a = false;
+                    break;
+                default:
+                    System.out.println("Selecione um modo de jogo válido");
+            }
+        }
+    }
+
+    private static void MenuIniciar() {
+        System.out.println("Bem vindo ao Masmorras e Dragões!");
+        System.out.println("Masmorras e Dragões é um jogo de estratégia por turnos onde apenas os mais habilidosos e destemidos sobreviverão!");
+        System.out.println("Neste combate tático, dois oponentes se enfrentam em uma Arena 2D. \nExistem três tipos de classe: Arqueiro, Guerreiro e Mago.");
+    }
 }
--- DueloDePersonagens/src/main/java/trabalho/duelodepersonagens/Menu.java
+++ DueloDePersonagens/src/main/java/trabalho/duelodepersonagens/Menu.java
@@ -2,9 +2,7 @@ package trabalho.duelodepersonagens;
 
 public class Menu {
     public Menu(){
-        System.out.println("Bem vindo ao Masmorras e Dragões!");
-        System.out.println("Masmorras e Dragões é um jogo de estratégia por turnos onde apenas os mais habilidosos e destemidos sobreviverão!");
-        System.out.println("Neste combate tático, dois oponentes se enfrentam em uma Arena 2D. \nExistem três tipos de classe: Arqueiro, Guerreiro e Mago.");
+
     }
 
 
